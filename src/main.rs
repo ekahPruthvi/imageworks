0bca04ef63f82ac2b576302eb6cf68d5283355a1
ekahPruthvi <p1sribhat@gmail.com>
2026-04-07 07:55:44 +0530

    fix working

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
 pub mod frame;
 
-use std::io;
-use crate::frame::image_to_matrix;
-
+use std::env;
+use std::process;
 
 fn main() {
-    let mat = frame::image_to_matrix("/home/ekah/save.png").expect("fail");
-    frame::matrix_to_terminal_show_thingy(&mat);
+    let args: Vec<String> = env::args().collect();
 
+    if args.len() < 3 {
+        print_usage();
+        process::exit(1);
+    }
+
+    let flag = &args[1];
+    let path = &args[2];
+
+    match flag.as_str() {
+        "-c" => {
+            println!("Compressing the image to `probe` at {}", path);
+        }
+        "-r" => {
+            println!("Reading `probe` file");
+        }
+        _ => {
+            eprintln!("Error: Unknown flag '{}'", flag);
+            print_usage();
+            process::exit(1);
+        }
+    }
+}
+
+fn print_usage() {
+    println!("Usage: imgrk [FLAG] [PATH]");
+    println!("\nFlags:");
+    println!("  -c    Compress the image to `probe` format");
+    println!("  -r    Read a `probe` file");
 }
